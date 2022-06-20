b11db8e4393a5b6d5ee90386d313b18619dad3e1
tenuko-1995 <kanasandoooo@gmail.com>
2022-06-21 06:48:39 +0900

    WPSのスコアを算出し、HTMLを書き換える

--- script.js
+++ script.js
@@ -4,19 +4,18 @@
 // 必要なElementを取得
 const countBtn = document.querySelector("#count-button");
 const wordsCount = document.querySelector(".words-count");
-
+let englishTextCount;
 //----------------   単語数を調べる   ----------------
 
 function countBtnClick() {
   // 空白文字で区切って配列に単語を格納。単語数を調べる
-  const englishTextCount = document
+  englishTextCount = document
     .querySelector("#english-text")
     .value.split(" ").length;
   // 単語数を表示
   wordsCount.innerHTML = `単語数: ${englishTextCount}`;
 }
 countBtn.addEventListener("click", countBtnClick);
-
 
 //----------------   タイマー機能   ----------------
 
@@ -27,7 +26,11 @@ let timerId = 0;
 // クリック時の時間を保持するための変数
 let startTime = 0;
 // ストップした時の時間を保持するための変数。ストップ→再開した時に0から始まるのを防ぐ
-let timeToadd = 0
+let timeToadd = 0;
+// 分
+let minutes = 0;
+// 秒
+let seconds = 0;
 
 // 必要なElementを取得
 const startBtn = document.querySelector("#start-button");
@@ -37,8 +40,10 @@ const timerElement = document.querySelector(".timer");
 
 // ミリ秒から分・秒に変換する関数
 function timeFunction() {
-  let minutes = Math.floor(elapsedTime / 60000);
-  let seconds = Math.floor((elapsedTime % 60000) / 1000);
+  // let minutes = Math.floor(elapsedTime / 60000);
+  // let seconds = Math.floor((elapsedTime % 60000) / 1000);
+  minutes = Math.floor(elapsedTime / 60000);
+  seconds = Math.floor((elapsedTime % 60000) / 1000);
 
   // HTMLを書き換える
   minutes = ("0" + minutes).slice(-2);
@@ -68,6 +73,12 @@ startBtn.addEventListener("mousedown", function () {
 stopBtn.addEventListener("mousedown", function () {
   clearTimeout(timerId);
   timeToadd += Date.now() - startTime;
+  //----------------   スコアを更新   ----------------
+  let timeTaken = (parseInt(minutes) * 60) + parseInt(seconds)
+  let score = Math.round((englishTextCount / timeTaken) * 60)
+  // Elementを取得し、書き換える
+  const resultValue = document.querySelector('.result-value')
+  resultValue.textContent = score;
 });
 
 // // リセットボタンをクリックした時の処理
@@ -76,4 +87,3 @@ resetBtn.addEventListener("mousedown", function () {
   timeToadd = 0;
   timeFunction();
 });
-
